74617cf5ab1133a630d4858bd865cfb9229656dd
Kayleigh <kayleighagordon@gmail.com>
2022-02-01 20:18:46 -0500

    Add clear button and functionality

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const grid = document.querySelector(".grid");
+const clearBtn = document.querySelector(".clear-btn");
 
-for(let i = 0; i < 16; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for(let j = 0; j < 16; j++){
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.addEventListener("mouseenter", () => {
-            cell.classList.add("hovered-cell")
+clearBtn.addEventListener("click", () => {
+    const rows = grid.childNodes;
+    for(let row of rows){
+        row.childNodes.forEach(cell => {
+            cell.classList.remove("hovered-cell");
         })
-        row.appendChild(cell);
     }
-    grid.appendChild(row);
+})
+
+function makeGrid(squaresPerSide = 16){
+    for(let i = 0; i < squaresPerSide; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < squaresPerSide; j++){
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.addEventListener("mouseenter", () => {
+                cell.classList.add("hovered-cell")
+            })
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
 }
+ makeGrid();
