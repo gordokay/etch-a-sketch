d1305ed6a58025309935ca0faec96cbaebb46a80
Kayleigh <kayleighagordon@gmail.com>
2022-02-01 20:44:23 -0500

    Add dynamic grid sizing

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const grid = document.querySelector(".grid");
 const clearBtn = document.querySelector(".clear-btn");
 
 clearBtn.addEventListener("click", () => {
-    const rows = grid.childNodes;
-    for(let row of rows){
-        row.childNodes.forEach(cell => {
-            cell.classList.remove("hovered-cell");
-        })
+    let squaresPerSide = prompt("Enter a number of grid squares per side (no more than 100)")
+    if(squaresPerSide > 100){
+        while(squaresPerSide > 100){
+            squaresPerSide = prompt("Enter a number less than 100");
+        }
     }
+    clearGrid();
+    makeGrid(squaresPerSide);
 })
 
 function makeGrid(squaresPerSide = 16){
+    
     for(let i = 0; i < squaresPerSide; i++){
         const row = document.createElement("div");
         row.classList.add("row");
@@ -25,4 +28,17 @@ function makeGrid(squaresPerSide = 16){
         grid.appendChild(row);
     }
 }
+
+function clearGrid(){
+    const rows = grid.childNodes;
+    rows.forEach(row => {
+        while(row.firstChild){
+            row.removeChild(row.firstChild);
+        }
+    })
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
+}
+
  makeGrid();
