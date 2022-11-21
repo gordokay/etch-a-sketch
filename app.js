a8839ec2531740a660125f8be16a065bf19a315b
Kayleigh <57508868+gordokay@users.noreply.github.com>
2022-11-21 14:46:04 -0500

    Add deleteGrid function and resizing feature

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-function makeGrid(cellNumber){
+const GRID_SIZE = 480;
+
+function makeGrid(cellNumber) {
   for(let i = 0; i < cellNumber; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     for(let j = 0; j < cellNumber; j++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
+      cell.style.width = `${GRID_SIZE / cellNumber}px`;
+      cell.style.height = `${GRID_SIZE / cellNumber}px`;
       row.appendChild(cell);
     }
     document.body.appendChild(row);
   }
+  makeDrawable();
 }
 
-function makeDrawable(){
+function deleteGrid() {
+  for(let row of document.querySelectorAll('.row')) {
+    for(let child of row.childNodes) {
+      row.removeChild(child);
+    }
+    document.body.removeChild(row);
+  }
+}
+ 
+function makeDrawable() {
   const cells = document.querySelectorAll('.cell');
   for(let cell of cells) {
     cell.addEventListener('mouseover', () => {
@@ -20,5 +34,14 @@ function makeDrawable(){
   }
 }
 
-makeGrid(16);
-makeDrawable();
+function etchASketch() {
+  const dimensionBtn = document.querySelector('button');
+  dimensionBtn.addEventListener('click', () => {
+    let dimension = prompt('Enter grid dimension');
+    deleteGrid();
+    makeGrid(dimension);
+  })
+  makeGrid(16);
+}
+
+etchASketch();
